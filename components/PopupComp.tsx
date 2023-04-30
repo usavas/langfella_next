0fdfe11288dc7915bfe1be9490c893d1455f351a
savas <savasusame@gmail.com>
2023-04-30 16:06:29 +0300

    ui changes

--- components/PopupComp.tsx
+++ components/PopupComp.tsx
@@ -39,23 +39,29 @@ const Popup = (props: Props) => {
       >
         <div
           onClick={handleInsideClick}
-          className="bg-white p-4  rounded-lg w-auto mx-8 max-w-lg"
+          className="bg-white p-4  rounded-lg w-auto mx-8 min-w-[20rem] max-w-lg"
         >
-          <h4 className="">Popup Title</h4>
-          <p className="text-gray-600 mb-4">{props.word}</p>
-
-          <p className="text-gray-600 mb-4">
-            {translation || "getting translation..."}
+          <h4 className=""></h4>
+          <p className="text-gray-600 mb-2 flex flex-row justify-between gap-2">
+            <span className="uppercase text-xs font-medium">word</span>
+            <span> {props.word} </span>
           </p>
-          <button className="bg-gray-500 text-white px-2 py-2 rounded-md mr-2">
-            Save Word
-          </button>
-          <button
-            className="bg-gray-400 text-white px-4 py-2 rounded-md"
-            onClick={close}
-          >
-            Close
-          </button>
+
+          <p className="text-gray-600 mb-2 flex flex-row justify-between gap-2">
+            <span className="uppercase text-xs font-medium">translation</span>
+            <span>{translation || "getting translation..."}</span>
+          </p>
+          <div className="flex flex-row gap-2">
+            <button className="bg-gray-500 text-white px-2 py-2 rounded-md flex-grow">
+              Save Word
+            </button>
+            <button
+              className="bg-gray-400 text-white px-4 py-2 rounded-md"
+              onClick={close}
+            >
+              Close
+            </button>
+          </div>
         </div>
       </div>
     </div>
