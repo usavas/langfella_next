e87f6e4b60df6ce550b2728e865e6665f494d14c
savas <savasusame@gmail.com>
2023-05-01 19:04:56 +0300

    mvoe type out of api to types folder

--- pages/api/fetchHtml.ts
+++ pages/api/fetchHtml.ts
@@ -2,31 +2,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import puppeteer, { ElementHandle, Page } from "puppeteer";
 
-type HtmlPage = {
-  pageUrl: string;
-  /**
-   * page title (in the address bar)
-   */
-  title?: string;
-  /**
-   * first h1 in the page, news title
-   */
-  headLine?: string;
-  /**
-   * page elements such as img, h1, h2, p, blockquote
-   * dump html content of ul, table and non-standard text nodes
-   */
-  elements: {
-    type: string;
-    /**
-     * text content for h1-h6, p, blockquote
-     * src for image,
-     * html dump for other elems such as ul, table etc.
-     */
-    content: string;
-  }[];
-};
-
 const fetchHtml = async (req: NextApiRequest, res: NextApiResponse) => {
   const { url } = req.query;
 
---
+++ types/api_types/HtmlPageResponse.ts
@@ -0,0 +1,24 @@
+type HtmlPage = {
+  pageUrl: string;
+  /**
+   * page title (in the address bar)
+   */
+  title?: string;
+  /**
+   * first h1 in the page, news title
+   */
+  headLine?: string;
+  /**
+   * page elements such as img, h1, h2, p, blockquote
+   * dump html content of ul, table and non-standard text nodes
+   */
+  elements: {
+    type: string;
+    /**
+     * text content for h1-h6, p, blockquote
+     * src for image,
+     * html dump for other elems such as ul, table etc.
+     */
+    content: string;
+  }[];
+};
