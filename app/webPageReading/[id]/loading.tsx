9157e6da221ffeebc1b58c87fe7a7ee6a28fc016
savas <savasusame@gmail.com>
2023-05-21 13:07:42 +0300

    add loading skeleton for a html page

---
+++ app/webPageReading/[id]/loading.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+function HtmlReadingLoadingSkeleton() {
+  return (
+    <div className="max-w-3xl mx-4 my-8">
+      <div className="w-full h-60 bg-gray-300 rounded-md mb-6 bg-gradient-to-tl transition-transform"></div>
+      <div className="w-full h-4 bg-gray-200 my-1"></div>
+      <div className="w-full h-4 bg-gray-200 my-1"></div>
+      <div className="w-40 h-4 bg-gray-200 my-1"></div>
+
+      <div className="w-full h-4 bg-gray-200 my-1"></div>
+      <div className="w-full h-4 bg-gray-200 my-1"></div>
+      <div className="w-40 h-4 bg-gray-200 my-1"></div>
+
+      <div className="w-full h-4 bg-gray-200 my-1"></div>
+      <div className="w-full h-4 bg-gray-200 my-1"></div>
+      <div className="w-40 h-4 bg-gray-200 my-1"></div>
+    </div>
+  );
+}
+
+export default HtmlReadingLoadingSkeleton;
