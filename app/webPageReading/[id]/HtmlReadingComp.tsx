b04cdedf1c657eaf260a72d3fbf73a4959fc122b
savas <savasusame@gmail.com>
2023-05-23 00:42:37 +0300

    select whole words on text selection on reading page

--- app/webPageReading/[id]/HtmlReadingComp.tsx
+++ app/webPageReading/[id]/HtmlReadingComp.tsx
@@ -41,7 +41,6 @@ function HtmlReadingComp({ webPage: reading }: Props) {
   return (
     <div
       className="max-w-3xl mx-4 my-8"
-      onClick={() => console.log("test")}
       onTouchEndCapture={handleSelection}
       onMouseUp={handleSelection}
     >
@@ -74,22 +73,101 @@ function HtmlReadingComp({ webPage: reading }: Props) {
    * @returns Handle text selection for both mouse and touch selection
    */
   function handleSelection(e: any): void {
-    const selectedString = window.getSelection()?.toString();
+    const selection = window.getSelection();
+    const selectedString = selection?.toString();
+
+    // selected nodes must be text nodes
+    if (
+      !IsSelectionValid(selection) ||
+      selection === null ||
+      selection.anchorNode === null ||
+      selection.anchorNode.textContent === null ||
+      selection.anchorNode !== selection.focusNode
+      // || !IsSelectionTextNode(selection)
+    ) {
+      return;
+    }
 
     // limit the word selection by 3 words for now.
     if (!selectedString || selectedString.split(" ").length > 3) {
       prevSelection = "";
       return;
     }
-    if (prevSelection === selectedString) {
+
+    // get start and end positions
+    let startPos = selection.anchorOffset;
+    let endPos = selection.focusOffset;
+    if (selection.anchorOffset - selection.focusOffset > 0) {
+      startPos = selection.focusOffset;
+      endPos = selection.anchorOffset;
+    }
+
+    // TODO disregard punctuations just like space (" ") char
+    while (startPos > 0 && getCharAt(startPos - 1) !== " ") {
+      if (getCharAt(startPos) === " ") {
+        startPos++;
+        break;
+      }
+      startPos--;
+    }
+
+    while (
+      endPos < selection.anchorNode.textContent.length - 1 &&
+      getCharAt(endPos) !== " "
+    ) {
+      if (getCharAt(endPos) === " ") {
+        console.log("trimmed");
+        endPos--;
+        break;
+      }
+
+      endPos++;
+      console.log(getCharAt(endPos));
+    }
+
+    // make the new selection
+    overrideLastSelectionOnWindow(selection, startPos, endPos);
+
+    const updatedSelectionString = window.getSelection()?.toString();
+
+    if (prevSelection === updatedSelectionString) {
       return;
     }
-    if (selectedString) {
-      prevSelection = selectedString;
+    if (updatedSelectionString) {
+      prevSelection = updatedSelectionString;
 
       // show popup for translation
-      setWordPopupSetting({ shown: true, word: selectedString });
+      setWordPopupSetting({ shown: true, word: updatedSelectionString });
     }
+
+    function getCharAt(i: number): string {
+      return selection!.anchorNode!.textContent!.charAt(i);
+    }
+
+    function IsSelectionValid(selection: Selection | null): boolean {
+      return (selection?.isCollapsed ?? false) === false;
+    }
+
+    function overrideLastSelectionOnWindow(
+      selection: Selection,
+      start: number,
+      end: number
+    ): void {
+      const range = document.createRange();
+      range.setStart(selection.anchorNode!, start);
+      range.setEnd(selection.anchorNode!, end);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+
+    // function IsSelectionTextNode(selection: Selection | null): boolean {
+    //   if (!selection) return false;
+
+    //   return (
+    //     (selection.focusNode?.TEXT_NODE ?? false) &&
+    //     (selection.anchorNode?.TEXT_NODE ?? false)
+    //   );
+    // }
   }
 
   function getTextNodeByTag(t: ContentItem): JSX.Element {
